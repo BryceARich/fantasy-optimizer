0512c85f9929725e2ab1fbd4c73b8d75791c68e3
BryceARich <brycerich17@yahoo.com>
2020-06-24 20:04:09 -0600

    chore: add recommendation table

--- fantasy_drafter/src/index.js
+++ fantasy_drafter/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {DrafteeTable} from './DrafteeTable'
+import {RecommendationTable} from './RecommendationTable'
 import {TeamsTable} from './TeamsTable'
 // import {calculateDeltasForPlayerToDraft, getTopPlayersOfEachPosition} from './api'
 
@@ -14,15 +15,6 @@ class FantasyDrafter extends React.Component{
         this.setState = this.setState.bind(this)
     }
 
-    addTeam(){
-        console.log("num teams:", this.state.numberOfTeams)
-        if(this.state.teams.length < 12){
-            this.setState(prevState => ({ teams: [...prevState.teams, '']}))
-        } else {
-            console.log("Max number of teams reached");
-        }
-    }
-
     render(){
         console.log(this.state)
         return(
@@ -30,16 +22,27 @@ class FantasyDrafter extends React.Component{
                 <TeamsTable teams={this.state.teams} numberOfTeams={this.state.numberOfTeams} setParentState={this.setState}/>
                 <br/>
                 <DrafteeTable playerRows={tempPlayerRows} teams={this.state.teams}/>
+                <br/>
+                <RecommendationTable playerRows={tempPlayerRows} teams={this.state.teams}/>
             </div>
         )
     }
 }
 
 function makePlayer(number){
+    const diffs = [];
+    let prev_diff = 0;
+    for(let i=0; i < 15; i++){
+        let thisDiff = Number(Math.random()*20) + prev_diff
+        console.log(prev_diff, thisDiff)
+        diffs.push(Number(parseFloat(thisDiff).toFixed(2)));
+        prev_diff = diffs[i];
+    }
     const player = {
         player: ("name: " + number),
         position: ("ps: " + number),
         fantasy_value: ("value: " + number),
+        diffs: diffs,
     }
     // console.log("made player " + number)
     return player
